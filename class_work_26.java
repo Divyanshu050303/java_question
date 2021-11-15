609d7957e67bddc0ef635719be81bdc7962c5f06
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-11-15 07:57:42 +0530

    print message

---
+++ class_work_26.java
@@ -0,0 +1,12 @@
+package com.company;
+class programming{
+    programming(String a){
+        System.out.println("I love "+a);
+    }
+}
+public class class_work_26 {
+    public static void main(String[] args) {
+        String a="Divyanshu";
+        programming m=new programming(a);
+    }
+}
